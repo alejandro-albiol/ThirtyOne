f8adc8cd3d0991f614ca1d78977da128e94885e7
Alejandro Albiol <albiolsafontalejandro@gmail.com>
2024-08-23 22:08:28 +0200

    Hand score function and added configuration variables

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const CARD_NUMBERS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const CARD_SUITS = ['hearts', 'diamonds', 'clubs', 'spades'];
 const NUMBER_OF_CARDS_IN_DECK = 52;
 const NUMBER_OF_CARDS_IN_HAND = 3;
 const INDEX_OF_USER_HAND = 0;
 const INDEX_OF_CPU_HAND = 1;
-const CARD_NUMBERS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-const CARD_SUITS = ['hearts', 'diamonds', 'clubs', 'spades'];
-
+const INDEX_OF_NUMBER_IN_CARD = 0;
+const INDEX_OF_SUIT_IN_CARD = 1;
+const VALUE_OF_J_Q_K_CARDS = 10;
+const VALUE_OF_AS_CARD = 11;
 
 function createCard(cardNumbers, cardSuits){
     let lastNumbersIndex = cardNumbers.length - 1
@@ -18,8 +21,6 @@ function createCard(cardNumbers, cardSuits){
 }
 
 function createDeck(numberOfCardsInDeck){
-    const NUMBER_INDEX = 0
-    const SUIT_INDEX = 1
     let deck = [];
 
     while (deck.length < numberOfCardsInDeck) {
@@ -27,7 +28,7 @@ function createDeck(numberOfCardsInDeck){
         let isTheCardInTheDeck = false;
 
         for (let i = 0; i < deck.length; i++) {
-            if (deck[i][NUMBER_INDEX] == newCard[NUMBER_INDEX] && deck[i][SUIT_INDEX] == newCard[SUIT_INDEX]) {
+            if (deck[i][INDEX_OF_NUMBER_IN_CARD] == newCard[INDEX_OF_NUMBER_IN_CARD] && deck[i][INDEX_OF_SUIT_IN_CARD] == newCard[INDEX_OF_SUIT_IN_CARD]) {
             isTheCardInTheDeck = true;
             break;
             }
@@ -40,11 +41,12 @@ function createDeck(numberOfCardsInDeck){
     return deck;
 }
 
-function createFirstHand(NUMBER_OF_CARDS_IN_HAND, deck){
+function createFirstHand(numberOfCardsInHand, deck){
     let playerHand = [];
     let cpuHand = [];
     let firstHand = [];
-    for(let i = 0; i < NUMBER_OF_CARDS_IN_HAND; i++){
+    
+    for(let i = 0; i < numberOfCardsInHand; i++){
         let drawPlayerCard = deck.pop()
         playerHand.push(drawPlayerCard);
         let drawCpuCard = deck.pop()
@@ -56,14 +58,41 @@ function createFirstHand(NUMBER_OF_CARDS_IN_HAND, deck){
     return firstHand;
 }
 
+function handScore(hand){
+    const INDEX_OF_FIRST_CARD_IN_HAND = 0
+    let cardPoints = 0;
+    let initialSuit = hand[INDEX_OF_FIRST_CARD_IN_HAND][INDEX_OF_SUIT_IN_CARD]
+
+    for(let i = 0; i < hand.length; i++){
+        let cardValue = hand[i][INDEX_OF_NUMBER_IN_CARD];
+        let cardSuit = hand[i][INDEX_OF_SUIT_IN_CARD];
+        if(initialSuit == cardSuit){
+            if(typeof cardValue == 'number'){
+                cardPoints += parseInt(hand[i]);
+            }else if(cardValue == 'J' || cardValue == 'Q' || cardValue == 'K'){
+                cardPoints += VALUE_OF_J_Q_K_CARDS;
+            }else{
+                cardPoints += VALUE_OF_AS_CARD;
+            }
+    }
+    }
+    return cardPoints;
+} 
+
 let deck = createDeck(NUMBER_OF_CARDS_IN_DECK);
-console.log(deck);
 
 let firstUserAndCpuHand = createFirstHand(NUMBER_OF_CARDS_IN_HAND, deck);
 
 let firstUserHand = firstUserAndCpuHand[INDEX_OF_USER_HAND];
 let firstCpuHand = firstUserAndCpuHand[INDEX_OF_CPU_HAND];
 
+let userHandScore = handScore(firstUserHand);
+let CpuHandScore = handScore(firstCpuHand);
+
+console.log("User hand:");
 console.log(firstUserHand);
+console.log("CPU hand:");
 console.log(firstCpuHand);
 console.log(deck);
+console.log(`User score: ${userHandScore}`);
+console.log(`CPU score: ${CpuHandScore}`);
